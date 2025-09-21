1f72bd1043357c6610742cfe1a2e2d18636f4cfa
Akbar ali <akbaralideveloper556778990@gmail.com>
2025-09-21 10:56:31 -0700

    init

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+const form = document.getElementById('form')
+const firstname = document.getElementById('firstname')
+const lastname = document.getElementById('lastname')
+const date = document.getElementById('date')
+const month = document.getElementById('month')
+const year = document.getElementById('year')
+const female = document.getElementById('female')
+const male = document.getElementById('male')
+const custom = document.getElementById('custom')
+const email = document.getElementById('email')
+const password = document.getElementById('password')

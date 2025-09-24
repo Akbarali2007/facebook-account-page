f22b99e7f926de59144ddbd81ee6e8c22150ab43
Akbar ali <akbaralideveloper556778990@gmail.com>
2025-09-23 22:05:15 -0700

    init

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 
-
-const form = document.getElementById('form')
-const firstname = document.getElementById('firstname')
-const lastname = document.getElementById('lastname')
-const date = document.getElementById('date')
-const month = document.getElementById('month')
-const year = document.getElementById('year')
-const female = document.getElementById('female')
-const male = document.getElementById('male')
-const custom = document.getElementById('custom')
-const email = document.getElementById('email')
-const password = document.getElementById('password')
+function submitform() {
+    const firstname = document.getElementById(firstName).value
+    const lastname = document.getElementById(lastName).value
+    const email =document.getElementById(Email).value
+    const password = document.getElementById(Password).value
+    const female = document.getElementById(Female).value
+    const male = document.getElementById(Male).value
+    const custom = document.getElementById(Custom).value
+    const Date = document.getElementById(date).value
+    const Month = document.getElementById(month).value
+    const Year = document.getElementById(year).value
+    
+    console.log(firstname,lastname,email,password,female,male,custom,Date,Month,Year)
+}
